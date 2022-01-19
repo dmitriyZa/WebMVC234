b771c855a9d0e01d95e331eb89a9f22107e9bef4
zagariy_ds <zagariy_ds@NL-L-7K311B3>
2022-01-19 09:52:46 +0300

    Добавьте файлы проекта.

---
+++ WebMVC234/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMVC234.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
